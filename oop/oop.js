35738ec2f81469b21d75642fae2ba1a99ea53243
juan267 <j.gomez267@gmail.com>
2016-09-15 16:37:23 -0500

    added cars example

--- oop/oop.js
+++ oop/oop.js
@@ -1,24 +1,62 @@
 //// OOP
 
-function Animal() {
+function Cat(eyeColor, furColor, breed) {
+  this.eyeColor = eyeColor
+  this.furColor = furColor
+  this.breed = breed
+  this.sleep = function() {
+    return 'zzzzzzzzzhmm pupu pur pur'
+  }
+  this.eat = function() {
+    return 'yum yum yum'
+  }
+}
 
+Cat.prototype.eat = function() {
+  return 'yum yum yum'
+}
+
+var perea = new Cat('cafes', 'negro', 'criollito')
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Animal(name, lastName) {
+  this.name = name
+  this.lastName = lastName
 }
 
 Animal.prototype.hello = function() {
   return 'Soy un animal'
 }
 
+Animal.prototype.fullName = function() {
+  return this.name + this.lastName
+}
+
 function Dog(name) {
   this.name = name
+  this.identify = function() {
+    return 'soy Constructora' + this.name
+  }
 }
 
-Dog.prototype = Object.create(Animal.prototype)
+// Dog.prototype = Object.create(Animal.prototype)
 
 Dog.prototype.identify = function() {
-  return 'soy ' + this.name
+  return 'soy protuptype' + this.name
 }
 
-var ozu = new Dog('Ozu')
+var ozu = new Dog('Ozu') // Instancias
 var moshi = new Dog('Moshi')
 
 moshi.bark = function() {
